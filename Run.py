d94a8eee5e07b7c0e0091b4c18ac02f13565a3e0
unknown <floatindigo218@gmail.com>
2022-03-06 21:49:18 +0900

    gnn update

--- Run.py
+++ Run.py
@@ -1,6 +1,7 @@
 import argparse
 
 from Loader import Loader
+from Model import *
 
 def parse_args():
     '''
@@ -16,5 +17,60 @@ def parse_args():
 
 class Run:
     def __init__(self):
-        loader = Loader()
+        self.loader = Loader()
         
+    def run_experiment(model, x_train, y_train, args):
+        # Compile the model.
+        model.compile(
+            optimizer=keras.optimizers.Adam(args.lr),
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+        )
+        # Create an early stopping callback.
+        early_stopping = keras.callbacks.EarlyStopping(
+            monitor="val_acc", patience=50, restore_best_weights=True
+        )
+        # Fit the model.
+        history = model.fit(
+            x=x_train,
+            y=y_train,
+            epochs=args.epochs,
+            batch_size=args.batch_size,
+            validation_split=0.15,
+            callbacks=[early_stopping],
+        )
+
+        return history
+    
+    def run(self,args):
+        citations, papers, x_train, x_test, y_train, y_test, train_data, test_data = self.loader.load_dataset()
+        feature_names = self.loader.feature_names
+        num_classes = self.loader.num_classes
+        graph_info = self.loader.graph_info
+        
+        self.gnn_model = GNNNodeClassifier(
+            graph_info=graph_info,
+            num_classes=num_classes,
+            hidden_units=[32,32],
+            dropout_rate=args.dropout_rate,
+            name="gnn_model",
+        )
+
+        # print("GNN output shape:", gnn_model([1, 10, 100]))
+        # gnn_model.summary()
+        
+        x_train = train_data.paper_id.to_numpy()
+        history = self.run_experiment(self.gnn_model, x_train, y_train)
+        
+    def test(self):
+        citations, papers, x_train, x_test, y_train, y_test, train_data, test_data = self.loader.load_dataset()
+        _, test_accuracy = self.gnn_model.evaluate(x=x_test, y=y_test, verbose=0)
+        print(f"Test accuracy: {round(test_accuracy * 100, 2)}%")
+
+if __name__=="__main__":
+    
+    args=parse_args()
+    run = Run()
+    gnn_moel = run.run(args)
+    
+    
